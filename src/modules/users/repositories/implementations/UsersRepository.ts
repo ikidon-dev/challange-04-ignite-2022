abb09c3afef4e56e01dbf1e44000d70cf7acca1d
João Antonio - Ikidon Dev <ikidon.dev@gmail.com>
2023-05-08 15:28:27 -0300

    challenge concluded!

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,44 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const date = new Date();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: date,
+      updated_at: date,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const userIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+    this.users[userIndex].admin = true;
+    this.users[userIndex].updated_at = new Date();
+    const user = this.users[userIndex];
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
